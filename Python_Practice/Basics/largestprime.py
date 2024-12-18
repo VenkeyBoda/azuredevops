592db4e3aecc5d2f2e73816bf08ae286fbd762db
venkeyboda07@gmail.com <venkeyboda07@gmail.com>
2024-12-18 10:34:09 +0530

    added largest prime

---
+++ Python_Practice/Basics/largestprime.py
@@ -0,0 +1,14 @@
+"""
+find the largest prime number
+"""
+number = 13195
+for factor in range(number-1,1,-1):
+    if number % factor == 0:
+       is_prime = True
+    for index in range(2, factor):
+        if factor % index == 0:
+          is_prime = False
+          break
+    if is_prime:
+           print(factor)
+           break
